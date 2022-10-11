a6d8b1c9495ea65ba015c965e9f70e6506d5db55
ijh4565 <ijh4565@naver.com>
2022-10-11 17:15:43 +0900

    fix: change WriteTransaction

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,17 @@ package main
 import(
 	"fmt"
 	"os"
-	"https://github.com/ijh4565/kafka_neo/kafka"
-	"https://github.com/ijh4565/kafka_neo/neo4j"
+	kafka "https://github.com/ijh4565/kafka_neo/pkg/kafka"
+	neo "https://github.com/ijh4565/kafka_neo/pkg/neo4j"
 )
 
 
 func main(){
+	flag := os.Args[1]
+
+	if flag == "K"{
+		kafka.Kappa("neo_connect")
+	} else if flag == "L"{
+		kafka.Lambda("neo_connect")
+	}
 }
--- pkg/neo4j/kappa.go
+++ pkg/neo4j/kappa.go
@@ -5,10 +5,6 @@ import (
 )
 
 func CreateItemKappa(tx neo4j.Transaction, con util.Info) error {
-	records, err := tx.Run("CREATE (n:Node {id: $id, name: $name })", map[string]interface{}{
-		"id":   con.Node1.Id,
-		"name": con.Node1.Name,
-	})
 	// Query 들어갈 자리
 
 	if err != nil {
@@ -24,7 +20,10 @@ func Neo4jWriteKappa(driver neo4j.Driver, info util.Info) {
 	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
 	defer session.Close()
 
-	result, err := session.WriteTransaction(CreateItemKappa(info))
+	result, err := session.RunRun("CREATE (n:Node {id: $id, name: $name })", map[string]interface{}{
+		"id":   con.Node1.Id,
+		"name": con.Node1.Name,
+	})
 	if err != nil {
 		panic(err)
 	}
