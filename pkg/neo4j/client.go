334ae9cf5f51d33a8b041ca092afbf87c0c30976
ijh4565 <ijh4565@naver.com>
2022-10-16 13:23:27 +0900

    add: Add Query Func

--- pkg/neo4j/client.go
+++ pkg/neo4j/client.go
@@ -2,6 +2,7 @@ package neo4j
 
 import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"log"
 )
 
 // Neo4J client
@@ -12,4 +13,23 @@ func Neo4JClient(username, password string) neo4j.Driver {
 		panic(err)
 	}
 	return driver
+}
+
+func Neo4jQuery(driver neo4j.Driver) {
+
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead})
+	defer session.Close()
+
+	result, err := session.Run("$Query", map[string]interface{}{
+		"nid1":   info.Node1.Id,
+		"name1":  info.Node1.Name,
+		"nid2":   info.Node2.Id,
+		"name2":  info.Node2.Name,
+		"eid1":   info.Edge.Id,
+		"weight": info.Edge.Weight,
+	})
+	if err != nil {
+		panic(err)
+	}
+	log.Println(result)
 }
